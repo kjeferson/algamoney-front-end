2341fe7433c4bc821459d2cebcce605c37f163e3
kjeferson <kjeferson@hotmail.com>
2023-02-11 13:20:07 -0300

    17.11. Alterando o locale da aplicação para pt-BR

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,13 @@
+import { registerLocaleData } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import localePt from '@angular/common/locales/pt';
+
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { TranslateLoader, TranslateModule, TranslateService } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 
 import { ConfirmationService, MessageService } from 'primeng/api';
 import { ConfirmDialogModule } from 'primeng/confirmdialog';
@@ -10,6 +17,12 @@ import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 import { LancamentosModule } from './lancamentos/lancamentos.module';
 import { PessoasModule } from './pessoas/pessoas.module';
+
+registerLocaleData(localePt, 'pt-BR');
+
+export function HttpLoaderFactory(http: HttpClient): TranslateHttpLoader {
+  return new TranslateHttpLoader(http);
+}
 
 @NgModule({
   declarations: [
@@ -24,11 +37,19 @@ import { PessoasModule } from './pessoas/pessoas.module';
     PessoasModule,
 
     ToastModule,
-    ConfirmDialogModule
+    ConfirmDialogModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient]
+      }
+    }),
   ],
   providers: [
     MessageService,
-    ConfirmationService
+    ConfirmationService,
+    TranslateService
   ],
   bootstrap: [AppComponent]
 })
